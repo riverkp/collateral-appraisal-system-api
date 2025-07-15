59ed894313aefb11a908f6a6aa88f3db192e15e3
sinyonar0623 <chinnaphon.trw@gmail.com>
2025-07-15 10:06:12 +0700

    Add CorrelationId in Task schema

--- Modules/Workflow/Task/Tasks/Models/Task.cs
+++ Modules/Workflow/Task/Tasks/Models/Task.cs
@@ -7,6 +7,7 @@ public class Task : Aggregate<long>
     public string TaskName { get; private set; } = default!;
     public string AssignedTo { get; private set; } = default!;
     public string AssignedType { get; private set; } = default!;
+    public string CorrelationId { get; private set; } = default!;
 
     private Task()
     {
@@ -16,20 +17,23 @@ public class Task : Aggregate<long>
     private Task(
         string taskName,
         string assignedTo,
-        string assignedType
+        string assignedType,
+        string correlationId
     )
     {
         TaskName = taskName;
         AssignedTo = assignedTo;
         AssignedType = assignedType;
+        CorrelationId = correlationId;
     }
 
     public static Task Create(
         string taskName,
         string assignedTo,
-        string assignedType
+        string assignedType,
+        string correlationId
     )
     {
-        return new Task(taskName, assignedTo, assignedType);
+        return new Task(taskName, assignedTo, assignedType, correlationId);
     }
 }

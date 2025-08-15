69174c239db59bef0fa415da41e01713ae8ccd55
sinyonar0623 <chinnaphon.trw@gmail.com>
2025-08-15 11:19:39 +0700

    Add PropertyBuilderExtension in Shared

--- Shared/Shared/Data/Extensions/PropertyBuilderExtension.cs
+++ Shared/Shared/Data/Extensions/PropertyBuilderExtension.cs
@@ -33,4 +33,34 @@ public static class PropertyBuilderExtension
     {
         return builder.HasPrecision(19, 4);
     }
+
+    public static PropertyBuilder<string?> UseShortStringConfig(this PropertyBuilder<string?> builder)
+    {
+        return builder.HasMaxLength(25);
+    }
+
+    public static PropertyBuilder<string?> UseSubStringConfig(this PropertyBuilder<string?> builder)
+    {
+        return builder.HasMaxLength(50);
+    }
+
+    public static PropertyBuilder<string?> UseNameConfig(this PropertyBuilder<string?> builder)
+    {
+        return builder.HasMaxLength(100);
+    }
+
+    public static PropertyBuilder<decimal?> UseAreaConfig(this PropertyBuilder<decimal?> builder)
+    {
+        return builder.HasPrecision(5, 2);
+    }
+
+    public static PropertyBuilder<decimal?> UseSizeConfig(this PropertyBuilder<decimal?> builder)
+    {
+        return builder.HasPrecision(7, 2);
+    }
+
+    public static PropertyBuilder<decimal?> UseLatLonConfig(this PropertyBuilder<decimal?> builder)
+    {
+        return builder.HasPrecision(9, 6);
+    }
 }

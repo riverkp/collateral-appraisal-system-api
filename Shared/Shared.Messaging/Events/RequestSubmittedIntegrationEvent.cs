43ec5d394923a4ec7e14510a397069b961ea23c9
sinyonar0623 <chinnaphon.trw@gmail.com>
2025-08-22 14:46:57 +0700

    Change Event Shared Name

--- Shared/Shared.Messaging/Events/RequestSubmittedIntegrationEvent.cs
+++ Shared/Shared.Messaging/Events/RequestSubmittedIntegrationEvent.cs
@@ -1,6 +1,9 @@
+using Shared.Dtos;
+
 namespace Shared.Messaging.Events;
 
 public record RequestSubmittedIntegrationEvent : IntegrationEvent
 {
     public long RequestId { get; set; } = default!;
+    public List<RequestTitleDto> RequestTitles {get; set;} = default!;
 }
--- Shared/Shared.Messaging/Events/RequestTitleSubmittedEvent.cs
+++
@@ -1,9 +0,0 @@
-using Shared.Dtos;
-
-namespace Shared.Messaging.Events;
-
-public record RequestTitleSubmittedEvent : IntegrationEvent
-{
-    public long RequestId { get; set; } = default!;
-    public List<RequestTitleDto> RequestTitles { get; set; } = default!;
-}
